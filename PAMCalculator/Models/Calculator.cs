04e38ef4f24373b8e2cf35628258ecb83ab30656
Danillo de Lacerda <danillo.lacerda77@gmail.com>
2024-09-27 22:48:41 -0300

    Projeto calculadora

---
+++ PAMCalculator/Models/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAMCalculator.Models
+{
+    public class Calculator
+    {
+        public double PrimeiroTermo { get; set; }
+        public double SegundoTermo2 { get; set; }
+        public double Resultado { get; set; }
+
+        public void Somar() { }
+
+        public void Subtrair() { }
+
+        public void Multiplicar() { }
+
+        public void Dividir() { }
+
+        public void Elevar() { }
+
+        public void Raiz() { }
+    }
+}
